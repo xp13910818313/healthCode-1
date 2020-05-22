b2c7822f0e36904bad9c251f71938a302fe8a633
Your Name <you@example.com>
2020-05-22 17:02:17 +0800

    updata5/22

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -3,11 +3,14 @@
 
 Page({
   data: {
-  
+    title:''
   },
 
-  onLoad: function() {
-   
+  onLoad: function(options) {
+    // 生命周期函数--监听页面加载
+    this.setData({
+      title: options.title
+    })
   },
 
 
